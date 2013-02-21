7c108f38ccac8127e3a7b00253660c7da545d730
Rutger Heijmerikx <rutger@thedutchlab.nl>
2013-02-21 08:30:38 +0100

    Do NOT copy/paste solutions

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var redis = require('redis');
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(":")[1]);
+  var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
 } else {
   var redisClient = redis.createClient();
 }
